17e8088d45e22d28465f257c4d07e3278d310738
AlvaroCap <alvaro.ruiz-navarro@capgemini.com>
2023-04-17 10:28:56 +0200

    Added the method event-list

--- src/app/components/events/event-list/event-list.component.ts
+++ src/app/components/events/event-list/event-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { Event } from 'src/app/models/event';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.scss']
+  styleUrls: ['./event-list.component.scss'],
 })
-export class EventListComponent {
+export class EventListComponent implements OnInit {
+  events: Event[] = [];
 
+  constructor(private router: Router, private eventService: EventService) {}
+
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe((data) => {
+      this.events = data;
+      console.log("Datos: " + data);
+    });
+    
+  }
 }
