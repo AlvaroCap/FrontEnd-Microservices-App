1b10b744bf629e8e7907a52947de0ba024db5970
JorgeLomincharJurado10 <jorge.lominchar-jurado@capgemini.com>
2023-04-17 08:24:16 +0200

    Creating Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserAddComponent } from './components/users/user-add/user-add.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAddComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/model/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  id: string;
+  name: string;
+  lastName: string;
+  mail: string;
+
+  constructor() {
+    this.id = '';
+    this.name = '';
+    this.lastName = '';
+    this.mail = '';
+  }
+}
