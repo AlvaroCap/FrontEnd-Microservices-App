857a101237e951a13c03e22ef08110f143718794
AlvaroCap <alvaro.ruiz-navarro@capgemini.com>
2023-04-18 08:51:15 +0200

    Modificado el app routing module para comenzar a desarrollar la funcion de Modificar Usuarios y Eventos

---
+++ src/app/components/events/event-modify/event-modify.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-modify',
+  templateUrl: './event-modify.component.html',
+  styleUrls: ['./event-modify.component.scss']
+})
+export class EventModifyComponent {
+
+}
